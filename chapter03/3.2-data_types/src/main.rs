cf1749ac9f5bede3964050d626d4a74497e4375d
Derek Wan <d.wan@icloud.com>
2021-11-22 07:51:39 +0800

    Add data-types

--- chapter03/3.2-data_types/src/main.rs
+++ chapter03/3.2-data_types/src/main.rs
@@ -1,7 +1,11 @@
-fn main() {
+use std::io;
+
+fn f1() {
+    // floating-point types
     let x = 2.; // f64
     let y: f32 = 3.0; //f32
 
+    // numeric operations
     // addition
     let sum = 5 + 10;
 
@@ -17,4 +21,67 @@ fn main() {
 
     // remainder
     let remainder = 43 % 5;
+
+    // the boolean type
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+
+    // the character type
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // the tuple type
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    // the array type
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    let a = [3; 5];
+    let first = a[0];
+    let second = a[1];
+}
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
