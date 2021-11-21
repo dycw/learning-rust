416ed0e25f8194849d38c85f34213f4ee58a0a40
Derek Wan <d.wan@icloud.com>
2021-11-22 07:44:19 +0800

    Move cargo up

---
+++ chapter03/3.2-data_types/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let x = 2.; // f64
+    let y: f32 = 3.0; //f32
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+
+    // remainder
+    let remainder = 43 % 5;
+}
