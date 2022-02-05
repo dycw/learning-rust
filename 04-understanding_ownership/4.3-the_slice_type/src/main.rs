ebaf289352c2f237957895a348deab332921f7b6
Derek Wan <d.wan@icloud.com>
2022-02-05 22:35:39 +0800

    more 4.3

--- 04-understanding_ownership/4.3-the_slice_type/src/main.rs
+++ 04-understanding_ownership/4.3-the_slice_type/src/main.rs
@@ -1,4 +1,6 @@
-fn first_word(s: &String) -> usize {
+// The Slice Type
+
+fn first_word_1(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
@@ -9,13 +11,120 @@ fn first_word(s: &String) -> usize {
 
     s.len()
 }
-fn main() {
+
+fn the_slice_type_1() {
     let mut s = String::from("hello world");
 
-    let word = first_word(&s);
+    let word = first_word_1(&s);
 
-    s.clear(); // this empties the String, making it equal to ""
+    s.clear();
+}
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
+// String Slices
+
+fn string_slices_1() {
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+}
+
+fn string_slices_2() {
+    let s = String::from("hello");
+
+    let slice = &s[0..2];
+    let slice = &s[..2];
+}
+
+fn string_slices_3() {
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[..];
+}
+
+fn string_slices_4() {
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+}
+
+fn first_word_2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn the_slice_type_2() {
+    let s = String::from("hello world");
+
+    let word = first_word_2(&s);
+
+    // s.clear(); // error!
+
+    println!("the first word is: {}", word);
+}
+
+// String Literals Are Slices
+
+fn string_literals_are_slices() {
+    let s = "Hello, world!";
+}
+
+// String Slices as Parameters
+
+fn first_word_3(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn the_slice_type_3() {
+    let my_string = String::from("hello world");
+
+    let word = first_word_3(&my_string[0..6]);
+    let word = first_word_3(&my_string[..]);
+    let word = first_word_3(&my_string);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word_3(&my_string_literal[0..6]);
+    let word = first_word_3(&my_string_literal[..]);
+
+    let word = first_word_3(my_string_literal);
+}
+
+// Other Slices
+
+fn other_slices_1() {
+    let a = [1, 2, 3, 4, 5];
+}
+
+fn other_slices_2() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3])
+}
+
+fn main() {
+    other_slices_2();
 }
