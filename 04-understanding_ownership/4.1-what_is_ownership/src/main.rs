9851a3f5735659cff460d5144ae9961aae6cc505
Derek Wan <d.wan@icloud.com>
2022-02-05 21:34:02 +0800

    Re-work through of 4.1 (#11)
    * Add 4.1
    
    * clean up more

--- 04-understanding_ownership/4.1-what_is_ownership/src/main.rs
+++ 04-understanding_ownership/4.1-what_is_ownership/src/main.rs
@@ -12,10 +12,16 @@ fn the_string_type_2() {
     println!("{}", s);
 }
 
-fn ways_variables_and_data_interact_move() {
+fn memory_and_allocation() {
+    let s = String::from("hello");
+}
+
+fn ways_variables_and_data_interact_move_1() {
     let x = 5;
     let y = x;
+}
 
+fn ways_variables_and_data_interact_move_2() {
     let s1 = String::from("hello");
     let s2 = s1;
 }
@@ -23,25 +29,18 @@ fn ways_variables_and_data_interact_move() {
 fn ways_variables_and_data_interact_clone() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
+
     println!("s1 = {}, s2 = {}", s1, s2);
 }
 
 fn stack_only_data_copy() {
     let x = 5;
     let y = x;
-    println!("x = {}, y= {}", x, y)
-}
-
-fn ownership_and_functions() {
-    let s = String::from("hello");
-    takes_ownership(s);
-
-    let x = 5;
-    makes_copy(5);
+    println!("x = {}, y = {}", x, y);
 }
 
 fn takes_ownership(some_string: String) {
-    println!("{}", some_string)
+    println!("{}", some_string);
 }
 
 fn makes_copy(some_integer: i32) {
@@ -49,9 +48,11 @@ fn makes_copy(some_integer: i32) {
 }
 
 fn return_values_and_scope_1() {
-    let s1 = gives_ownership();
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
+    let s = String::from("hello");
+    takes_ownership(s);
+
+    let x = 5;
+    makes_copy(x);
 }
 
 fn gives_ownership() -> String {
@@ -64,9 +65,9 @@ fn takes_and_gives_back(a_string: String) -> String {
 }
 
 fn return_values_and_scope_2() {
-    let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is {}.", s2, len)
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
 }
 
 fn calculate_length(s: String) -> (String, usize) {
@@ -74,6 +75,12 @@ fn calculate_length(s: String) -> (String, usize) {
     (s, length)
 }
 
+fn return_values_and_scope_3() {
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len)
+}
+
 fn main() {
-    return_values_and_scope_2();
+    return_values_and_scope_3()
 }
