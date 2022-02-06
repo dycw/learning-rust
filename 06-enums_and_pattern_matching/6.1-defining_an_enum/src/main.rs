e8cc58721588120bbb6f9f98232926de4dae9440
Derek Wan <d.wan@icloud.com>
2022-02-06 10:06:03 +0800

    simplify

--- 06-enums_and_pattern_matching/6.1-defining_an_enum/src/main.rs
+++ 06-enums_and_pattern_matching/6.1-defining_an_enum/src/main.rs
@@ -36,6 +36,30 @@ fn enum_values_3() {
     };
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage;
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String);
+struct ChangeColorMessage(i32, i32, i32);
+
+impl Message {
+    fn call(&self) {}
+}
+
+fn enum_values_4() {
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+
 fn main() {
-    enum_values_3();
+    enum_values_4();
 }
