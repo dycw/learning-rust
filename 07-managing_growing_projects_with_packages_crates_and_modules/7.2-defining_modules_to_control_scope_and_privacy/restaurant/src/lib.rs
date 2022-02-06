436afa7e8a493fd190a34eefc8ed28aa500ca860
Derek Wan <d.wan@icloud.com>
2022-02-06 18:00:37 +0800

    Add 7.3

--- 07-managing_growing_projects_with_packages_crates_and_modules/7.2-defining_modules_to_control_scope_and_privacy/restaurant/src/lib.rs
+++ 07-managing_growing_projects_with_packages_crates_and_modules/7.2-defining_modules_to_control_scope_and_privacy/restaurant/src/lib.rs
@@ -13,12 +13,3 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
