dba3e27b4b1287782280465ab832e737ba813bbc
Derek Wan <d.wan@icloud.com>
2022-02-08 07:50:22 +0800

    Add 8.3

---
+++ 08-common_collections/8.3-storing_keys_with_associated_values_in_hash_maps/src/main.rs
@@ -0,0 +1,106 @@
+// Creating a New Hash Map
+
+fn creating_a_new_hash_map_1() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
+
+fn creating_a_new_hash_map_2() {
+    use std::collections::HashMap;
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+}
+
+// Hash Maps and Ownership
+
+fn hash_maps_and_ownership() {
+    use std::collections::HashMap;
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+}
+
+// Accessing Values in a Hash Map
+
+fn accessing_values_in_a_hash_map_1() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+}
+
+fn accessing_values_in_a_hash_map_2() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+}
+
+// Overwriting a Value
+
+fn overwriting_a_value() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+}
+
+// Only Inserting a Value If the Key Has No Value
+
+fn only_inserting_a_value_if_the_key_has_no_value() {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+}
+
+// Updating a Value Based on the Old Value
+
+fn updating_a_value_based_on_the_old_value() {
+    use std::collections::HashMap;
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
+
+fn main() {
+    updating_a_value_based_on_the_old_value();
+}
