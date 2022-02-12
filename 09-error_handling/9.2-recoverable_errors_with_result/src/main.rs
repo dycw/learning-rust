5d7e073fe5a179d6476836f08062681e2d444579
Derek Wan <d.wan@icloud.com>
2022-02-12 21:46:53 +0800

    Add 9.2

--- 09-error_handling/9.2-recoverable_errors_with_result/src/main.rs
+++ 09-error_handling/9.2-recoverable_errors_with_result/src/main.rs
@@ -1,8 +1,6 @@
 // Recoverable Errors with Result
 
-use core::panic;
-use core::panicking::panic;
-use std::fs::File;
+use std::fs::{self, File};
 
 fn recoverable_errors_with_result_1() {
     let f = File::open("recoverable_errors_with_result_1.txt");
@@ -50,6 +48,77 @@ fn matching_on_different_errors_2() {
     });
 }
 
-fn main() {
-    matching_on_different_errors_2();
+// Shortcuts for Panic on Error: unwrap and expect
+
+fn shortcuts_for_panic_on_error_unwrap_and_expect_1() {
+    let f = File::open("shortcuts_for_panic_on_error_unwrap_and_expect_1.txt").unwrap();
+}
+
+fn shortcuts_for_panic_on_error_unwrap_and_expect_2() {
+    let f = File::open("shortcuts_for_panic_on_error_unwrap_and_expect_2.txt")
+        .expect("Failed to open shortcuts_for_panic_on_error_unwrap_and_expect_2.txt");
+}
+
+// Propagating Errors
+
+use std::io::{self, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("read_username_from_file.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+// A Shortcut for Propagating Errors: the ? Operator
+
+fn a_shortcut_for_propagating_errors_the_question_mark_operator_1() -> Result<String, io::Error> {
+    let mut f = File::open("a_shortcut_for_propagating_errors_the_question_mark_operator_1.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn a_shortcut_for_propagating_errors_the_question_mark_operator_2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("a_shortcut_for_propagating_errors_the_question_mark_operator_2.txt")?
+        .read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn a_shortcut_for_propagating_errors_the_question_mark_operator_3() -> Result<String, io::Error> {
+    fs::read_to_string("a_shortcut_for_propagating_errors_the_question_mark_operator_3.txt")
+}
+
+// Where The ? Operator Can Be Used
+
+// fn where_the_question_mark_operator_can_be_used_1() {
+//     let f = File::open("where_the_question_mark_operator_can_be_used.txt")?;
+// }
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
+}
+
+use std::error::Error;
+
+fn where_the_question_mark_operator_can_be_used_2() -> Result<(), Box<dyn Error>> {
+    let f = File::open("where_the_question_mark_operator_can_be_used_2.txt")?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    where_the_question_mark_operator_can_be_used_2()
 }
