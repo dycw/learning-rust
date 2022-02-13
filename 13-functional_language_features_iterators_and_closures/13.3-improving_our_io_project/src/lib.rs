cd3ed53d94b393b303c59c8135b208af61653332
Derek Wan <d.wan@icloud.com>
2022-02-14 07:43:41 +0800

    Finish Chapter 13 (#23)
    * Setup
    
    * Finish 13.3
    
    * Add 13.4

---
+++ 13-functional_language_features_iterators_and_closures/13.3-improving_our_io_project/src/lib.rs
@@ -0,0 +1,101 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+// Using the Returned Iterator Directly
+// Using Iterator Trait Methods Instead of Indexing
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn run_1(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search_1(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search_1<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+// Making Code Clearer with Iterator Adaptors
+
+pub fn search_2<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+pub fn run_2(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search_2(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
--- 13-functional_language_features_iterators_and_closures/13.3-improving_our_io_project/src/main.rs
+++ 13-functional_language_features_iterators_and_closures/13.3-improving_our_io_project/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, process};
+
+use improving_our_io_project::{run_2, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run_2(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
