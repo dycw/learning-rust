0e249663fc8a1bc0a8224ded10c2195e3e038e20
Derek Wan <d.wan@icloud.com>
2022-02-13 23:02:20 +0800

    Setup

--- 13-functional_language_features_iterators_and_closures/13.3-improving_our_io_project/src/main.rs
+++ 13-functional_language_features_iterators_and_closures/13.3-improving_our_io_project/src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, process};
+
+use improving_our_io_project::{run, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
