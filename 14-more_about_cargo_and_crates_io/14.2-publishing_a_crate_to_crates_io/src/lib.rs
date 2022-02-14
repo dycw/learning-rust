1dc54737efd34050bf1d5d3160dc5c388005d08f
Derek Wan <d.wan@icloud.com>
2022-02-14 08:03:36 +0800

    Commit

---
+++ 14-more_about_cargo_and_crates_io/14.2-publishing_a_crate_to_crates_io/src/lib.rs
@@ -0,0 +1,50 @@
+//! # My Crate
+//!
+//! `publishing_a_crate_to_crates_io` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = publishing_a_crate_to_crates_io::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+// Exporting a Convenient Public API with pub use
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+    }
+}
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
---
+++ 14-more_about_cargo_and_crates_io/14.2-publishing_a_crate_to_crates_io/src/main.rs
@@ -0,0 +1,21 @@
+use publishing_a_crate_to_crates_io::kinds::PrimaryColor;
+use publishing_a_crate_to_crates_io::utils::mix;
+
+fn exporting_a_convenient_public_api_with_pub_use_1() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
+
+use publishing_a_crate_to_crates_io::mix;
+use publishing_a_crate_to_crates_io::PrimaryColor;
+
+fn exporting_a_convenient_public_api_with_pub_use_2() {
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    mix(red, yellow);
+}
+
+fn main() {
+    exporting_a_convenient_public_api_with_pub_use_2();
+}
