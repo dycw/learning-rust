414091277a9ffc789b50e00b85968259e7ad6dd0
Derek Wan <d.wan@icloud.com>
2022-02-20 00:51:05 +0800

    Add Chapter 18 (#30)
    * Add Chapter 18.1
    
    * Add Chapter 17.2
    
    * Finish

---
+++ 18-patterns_and_matching/18.2-refutability_whether_a_pattern_might_fail_to_match/src/main.rs
@@ -0,0 +1,11 @@
+// Refutability: Whether a Pattern Might Fail to Match
+
+fn refutability_whether_a_pattern_might_fail_to_match() {
+    if let x = 5 {
+        println!("{}", x);
+    }
+}
+
+fn main() {
+    refutability_whether_a_pattern_might_fail_to_match();
+}
