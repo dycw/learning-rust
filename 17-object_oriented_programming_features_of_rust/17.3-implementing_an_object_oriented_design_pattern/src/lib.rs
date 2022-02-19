9cae5f44d014c930b870033441343da7b8a621e9
Derek Wan <d.wan@icloud.com>
2022-02-19 21:58:18 +0800

    Start 17.3

---
+++ 17-object_oriented_programming_features_of_rust/17.3-implementing_an_object_oriented_design_pattern/src/lib.rs
@@ -0,0 +1,21 @@
+// Defining Post and Creating a New Instance in the Draft State
+
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+trait State {}
+
+struct Draft {}
+
+impl State for Draft {}
