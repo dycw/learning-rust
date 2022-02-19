a86e044c548d65e11ec4ee3707defc0d63eed907
Derek Wan <d.wan@icloud.com>
2022-02-19 22:53:40 +0800

    Finish 17.3

--- 17-object_oriented_programming_features_of_rust/17.3-implementing_an_object_oriented_design_pattern/src/lib.rs
+++ 17-object_oriented_programming_features_of_rust/17.3-implementing_an_object_oriented_design_pattern/src/lib.rs
@@ -74,14 +74,223 @@ struct Draft2 {}
 
 impl State2 for Draft2 {
     fn request_review(self: Box<Self>) -> Box<dyn State2> {
-        Box::new(PendingReview {})
+        Box::new(PendingReview2 {})
     }
 }
 
-struct PendingReview {}
+struct PendingReview2 {}
 
-impl State2 for PendingReview {
+impl State2 for PendingReview2 {
     fn request_review(self: Box<Self>) -> Box<dyn State2> {
         self
+    }
+}
+
+// Adding the approve Method that Changes the Behavior of content
+
+pub struct Post3 {
+    state: Option<Box<dyn State3>>,
+    content: String,
+}
+
+impl Post3 {
+    pub fn new() -> Post3 {
+        Post3 {
+            state: Some(Box::new(Draft3 {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        ""
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State3 {
+    fn request_review(self: Box<Self>) -> Box<dyn State3>;
+    fn approve(self: Box<Self>) -> Box<dyn State3>;
+}
+
+struct Draft3 {}
+
+impl State3 for Draft3 {
+    fn request_review(self: Box<Self>) -> Box<dyn State3> {
+        Box::new(PendingReview3 {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State3> {
+        self
+    }
+}
+
+struct PendingReview3 {}
+
+impl State3 for PendingReview3 {
+    fn request_review(self: Box<Self>) -> Box<dyn State3> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State3> {
+        Box::new(Published3 {})
+    }
+}
+
+struct Published3 {}
+
+impl State3 for Published3 {
+    fn request_review(self: Box<Self>) -> Box<dyn State3> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State3> {
+        self
+    }
+}
+
+pub struct Post4 {
+    state: Option<Box<dyn State4>>,
+    content: String,
+}
+
+impl Post4 {
+    pub fn new() -> Post4 {
+        Post4 {
+            state: Some(Box::new(Draft4 {})),
+            content: String::new(),
+        }
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+}
+
+trait State4 {
+    fn request_review(self: Box<Self>) -> Box<dyn State4>;
+    fn approve(self: Box<Self>) -> Box<dyn State4>;
+    fn content<'a>(&self, post: &'a Post4) -> &'a str {
+        ""
+    }
+}
+
+struct Draft4 {}
+
+impl State4 for Draft4 {
+    fn request_review(self: Box<Self>) -> Box<dyn State4> {
+        Box::new(PendingReview4 {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State4> {
+        self
+    }
+}
+
+struct PendingReview4 {}
+
+impl State4 for PendingReview4 {
+    fn request_review(self: Box<Self>) -> Box<dyn State4> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State4> {
+        Box::new(Published4 {})
+    }
+}
+
+struct Published4 {}
+
+impl State4 for Published4 {
+    fn request_review(self: Box<Self>) -> Box<dyn State4> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State4> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post4) -> &'a str {
+        &post.content
+    }
+}
+
+// Encoding States and Behavior as Types
+
+pub struct Post5 {
+    content: String,
+}
+
+pub struct DraftPost5 {
+    content: String,
+}
+
+impl Post5 {
+    pub fn new() -> DraftPost5 {
+        DraftPost5 {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost5 {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+}
+
+// Implementing Transitions as Transformations into Different Types
+
+impl DraftPost5 {
+    pub fn request_review(self) -> PendingReviewPost5 {
+        PendingReviewPost5 {
+            content: self.content,
+        }
+    }
+}
+
+pub struct PendingReviewPost5 {
+    content: String,
+}
+
+impl PendingReviewPost5 {
+    pub fn approve(self) -> Post5 {
+        Post5 {
+            content: self.content,
+        }
     }
 }
