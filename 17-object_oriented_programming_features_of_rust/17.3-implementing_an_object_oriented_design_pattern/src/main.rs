24eebeb76f1705ad85e9fcb61a2cdc2e7513f4c9
Derek Wan <d.wan@icloud.com>
2022-02-19 22:56:22 +0800

    Add Chapter 17.3 (#29)
    * Start 17.3
    
    * wip
    
    * Finish 17.3

--- 17-object_oriented_programming_features_of_rust/17.3-implementing_an_object_oriented_design_pattern/src/main.rs
+++ 17-object_oriented_programming_features_of_rust/17.3-implementing_an_object_oriented_design_pattern/src/main.rs
@@ -1,9 +1,9 @@
 // Implementing an Object-Oriented Design Pattern
 
-use implementing_an_object_oriented_design_pattern::Post2;
+use implementing_an_object_oriented_design_pattern::Post4;
 
 fn implementing_an_object_oriented_design_pattern() {
-    let mut post = Post2::new();
+    let mut post = Post4::new();
 
     post.add_text("I ate a salad for lunch today");
     assert_eq!("", post.content());
@@ -15,6 +15,22 @@ fn implementing_an_object_oriented_design_pattern() {
     assert_eq!("I ate a salad for lunch today", post.content());
 }
 
+// Encoding States and Behavior as Types
+
+use implementing_an_object_oriented_design_pattern::Post5;
+
+fn encoding_states_and_behavior_as_types() {
+    let mut post = Post5::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
 fn main() {
-    implementing_an_object_oriented_design_pattern();
+    encoding_states_and_behavior_as_types();
 }
