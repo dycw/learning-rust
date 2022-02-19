2ecf67b3fc02e904428ea76c8daa28647100a696
Derek Wan <d.wan@icloud.com>
2022-02-19 20:20:39 +0800

    Add Chapter 16.2

---
+++ 16-fearless_concurrency/16.2-using_message_passing_to_transfer_data_between_threads/src/main.rs
@@ -0,0 +1,108 @@
+// Using Message Passing to Transfer Data Between Threads
+
+use std::sync::mpsc;
+use std::thread;
+
+fn using_message_passing_to_transfer_data_between_threads_1() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+}
+
+fn using_message_passing_to_transfer_data_between_threads_2() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+// Channels and Ownership Transference
+
+fn channels_and_ownership_transference() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // println!("val is {}", val);
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+// Sending Multiple Values and Seeing the Receiver Waiting
+
+use std::time::Duration;
+
+fn sending_multiple_values_and_seeing_the_receiver_waiting() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+
+// Creating Multiple Producers by Cloning the Transmitter
+
+fn creating_multiple_producers_by_cloning_the_transmitter() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
+fn main() {
+    creating_multiple_producers_by_cloning_the_transmitter();
+}
