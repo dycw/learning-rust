f9a1593a6b27c0ec657f343ad6eac804093b89a1
Derek Wan <d.wan@icloud.com>
2022-02-19 11:36:30 +0800

    Add Chapter 15.1

--- 15-smart_pointers/15.1-using_box_t_to_point_to_data_on_the_heap/src/main.rs
+++ 15-smart_pointers/15.1-using_box_t_to_point_to_data_on_the_heap/src/main.rs
@@ -5,6 +5,19 @@ fn using_a_box_t_to_point_to_data_on_the_heap() {
     println!("b = {}", b);
 }
 
+// More Information About the Cons List
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+fn more_information_about_the_cons_list() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
+
 fn main() {
     using_a_box_t_to_point_to_data_on_the_heap()
 }
