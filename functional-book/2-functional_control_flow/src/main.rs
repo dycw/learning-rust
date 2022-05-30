f2fab64a21980f68b8b6de2be77a12d13a775e4b
Derek Wan <d.wan@icloud.com>
2022-05-30 22:28:33 +0800

    working through

--- functional-book/2-functional_control_flow/src/main.rs
+++ functional-book/2-functional_control_flow/src/main.rs
@@ -1,21 +1,62 @@
+use std::{
+    env,
+    fs::File,
+    io::{self, Read},
+};
+
 pub fn run_simulation() {
     // 1. Store location, velocity, and acceleration state
     let mut location: f64 = 0.0; // meters
     let mut velocity: f64 = 0.0; // meters per second
     let mut acceleration: f64 = 0.0; // meters per second squared
 
-    //2. Store motor input voltage
+    // 2. Store motor input voltage
     let mut up_input_voltage: f64 = 0.0;
     let mut down_input_voltage: f64 = 0.0;
 
-    //3. Store input building description and floor requests
+    // 3. Store input building description and floor requests
     let mut floor_count: u64 = 0;
     let mut floor_height: f64 = 0.0; // meters
     let mut floor_requests: Vec<u64> = Vec::new();
 
-    //4. Parse input and store as building description and floor requests
+    // 4. Parse input and store as building description and floor requests
+    let buffer = match env::args().nth(1) {
+        Some(ref fp) if *fp == "-".to_string() => {
+            let mut buffer = String::new();
+            io::stdin()
+                .read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        }
+        None => {
+            let fp = "test1.txt";
+            let mut buffer = String::new();
+            File::open(fp)
+                .expect("File::open failed")
+                .read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        }
+        Some(fp) => {
+            let mut buffer = String::new();
+            File::open(fp)
+                .expect("File::open failed")
+                .read_to_string(&mut buffer)
+                .expect("read_to_string failed");
+            buffer
+        }
+    };
+    for (li, l) in buffer.lines().enumerate() {
+        if li == 0 {
+            floor_count = l.parse::<u64>().unwrap();
+        } else if li == 1 {
+            floor_height = l.parse::<f64>().unwrap();
+        } else {
+            floor_requests.push(l.parse::<u64>().unwrap());
+        }
+    }
 
-    //5. Loop while there are remaining floor requests
+    // 5. Loop while there are remaining floor requests
     while floor_requests.len() > 0 {
         //5.1. Update location, velocity, and acceleration
         //5.2. If next floor request in queue is satisfied, then remove from queue
