e9fdc61108b073acafed3a8168dd5749cd931d37
Derek Wan <d.wan@icloud.com>
2022-05-31 06:42:47 +0800

    Finish chapter 2

---
+++ functional-book/2-functional_control_flow/tests/integration_tests.rs
@@ -0,0 +1,13 @@
+extern crate elevator;
+extern crate timebomb;
+use timebomb::timeout_ms;
+
+#[test]
+fn test_main() {
+    timeout_ms(
+        || {
+            elevator::run_simulation();
+        },
+        300000,
+    );
+}
