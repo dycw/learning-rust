049ee750cfe44477df51735603609c0c04bb40fe
Derek Wan <d.wan@icloud.com>
2022-06-02 07:10:49 +0800

    Finish chapter 2

---
+++ beginning-book/2-doing_arithmetic_and_writing_more_code/11/src/main.rs
@@ -0,0 +1,6 @@
+#[rustfmt::skip]
+fn main() {
+       print!("{} + ",80);print!("{} = ",34);
+            print  ! ( "{}"  ,
+        80     + 34 )  ;
+}
---
+++ beginning-book/2-doing_arithmetic_and_writing_more_code/13/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!(
+        "{}",
+        "These
+        are
+        three lines"
+    );
+}
