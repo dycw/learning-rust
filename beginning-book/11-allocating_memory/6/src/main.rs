c80841b87c71e922c7ee8d962549bdd341148890
Derek Wan <d.wan@icloud.com>
2022-06-02 22:56:09 +0800

    Finish chapter 11 (#50)

---
+++ beginning-book/11-allocating_memory/6/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    fn f(p: &f64) {
+        let a = Box::new(*p);
+        {
+            let b = Box::new([1, 2, 3]);
+            print!("{} {:?}", *a, *b);
+        }
+        let c = Box::new(true);
+        print!(" {} {}", a, c);
+    }
+    f(&3.4);
+}
