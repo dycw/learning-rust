0130788cc5786f16e26ec4adb87029c5ade81f5d
Derek Wan <d.wan@icloud.com>
2022-06-03 16:00:20 +0800

    Finish chapter 24

--- beginning-book/24-more_about_lifetimes/15/src/main.rs
+++ beginning-book/24-more_about_lifetimes/15/src/main.rs
@@ -1,3 +1,15 @@
+// In some library code:
+struct S<'a> {
+    b: bool,
+    ri: &'a i32,
+}
+fn create_s<'b>(ri: &'b i32) -> S<'b> {
+    S { b: true, ri: ri }
+}
+// In application code:
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 12;
+    let y: S;
+    y = create_s(&x);
+    print!("{} {}", y.b, *y.ri);
 }
