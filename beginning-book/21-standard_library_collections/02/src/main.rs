625c10be019a5974d77652037f097b4ad760f096
Derek Wan <d.wan@icloud.com>
2022-06-03 14:45:23 +0800

    Do 1-5

--- beginning-book/21-standard_library_collections/02/src/main.rs
+++ beginning-book/21-standard_library_collections/02/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    use std::time::Instant;
+    const SIZE: usize = 100_000_000;
+    let start_time = Instant::now();
+    let mut v = Vec::<usize>::with_capacity(SIZE);
+    let t1 = start_time.elapsed();
+    for i in 0..SIZE {
+        v.push(i);
+    }
+    let t2 = start_time.elapsed();
+    for _ in 0..SIZE {
+        v.pop();
+    }
+    let t3 = start_time.elapsed();
+    print!("{:?} {:?} {:?}", t1, t2 - t1, t3 - t2);
 }
--- beginning-book/21-standard_library_collections/03/src/main.rs
+++ beginning-book/21-standard_library_collections/03/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    use std::time::Instant;
+    const SIZE: usize = 100_000;
+    let start_time = Instant::now();
+    let mut v = Vec::<usize>::with_capacity(SIZE);
+    let t1 = start_time.elapsed();
+    for i in 0..SIZE {
+        v.insert(0, i);
+    }
+    let t2 = start_time.elapsed();
+    for _ in 0..SIZE {
+        v.remove(0);
+    }
+    let t3 = start_time.elapsed();
+    print!("{:?} {:?} {:?}", t1, t2 - t1, t3 - t2);
 }
