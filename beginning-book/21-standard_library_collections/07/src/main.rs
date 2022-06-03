5284ba297aaa09a6dc33e76502e096813366e6da
Derek Wan <d.wan@icloud.com>
2022-06-03 14:51:50 +0800

    Finish chapter 21 (#60)

---
+++ beginning-book/21-standard_library_collections/07/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    use std::time::Instant;
+    const SIZE: usize = 40_000;
+    let mut v = Vec::<usize>::new();
+    let mut vd = std::collections::VecDeque::<usize>::new();
+    let start_time = Instant::now();
+    for i in 0..SIZE {
+        v.push(i);
+    }
+    let t1 = start_time.elapsed();
+    for i in 0..SIZE {
+        vd.push_back(i);
+    }
+    let mut count = 0;
+    let t2 = start_time.elapsed();
+    for i in v.iter() {
+        count += i;
+    }
+    let t3 = start_time.elapsed();
+    for i in vd.iter() {
+        count += i;
+    }
+    let t4 = start_time.elapsed();
+    print!(
+        "{} {:?} {:?} {:?} {:?}",
+        count,
+        t1,
+        t2 - t1,
+        t3 - t2,
+        t4 - t3
+    );
+}
