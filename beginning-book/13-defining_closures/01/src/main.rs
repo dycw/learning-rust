75eb61f24230b41ee2403bf2bf1160ab6fd068e4
Derek Wan <d.wan@icloud.com>
2022-06-03 09:44:15 +0800

    Finish chapter 13 (#52)

---
+++ beginning-book/13-defining_closures/01/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let mut arr = [4, 8, 1, 10, 0, 45, 12, 7];
+    arr.sort();
+    print!("{:?}", arr);
+}
---
+++ beginning-book/13-defining_closures/06/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut arr = [4, 8, 1, 10, 0, 45, 12, 7];
+    use std::cmp::Ordering;
+    let desc = |a: &i32, b: &i32| -> Ordering {
+        if a < b {
+            Ordering::Greater
+        } else if a > b {
+            Ordering::Less
+        } else {
+            Ordering::Equal
+        }
+    };
+    arr.sort_by(desc);
+    print!("{:?}", arr);
+}
