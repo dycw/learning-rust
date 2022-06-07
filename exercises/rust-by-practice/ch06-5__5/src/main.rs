7d33dbc755a11ee4af3af2c5ca109cf033036df9
Derek Wan <d.wan@icloud.com>
2022-06-07 21:31:38 +0800

    Finish chapter 6 (#92)

---
+++ exercises/rust-by-practice/ch06-5__5/src/main.rs
@@ -0,0 +1,13 @@
+// Fill the blank
+struct Person {
+    name: String,
+    age: u8,
+}
+
+fn main() {
+    println!("Success!");
+}
+
+fn build_person(name: String, age: u8) -> Person {
+    Person { age, name }
+}
