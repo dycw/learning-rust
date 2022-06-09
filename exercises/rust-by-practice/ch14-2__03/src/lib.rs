ef9795e7d15c284111f80ec2d2e717963903fd8e
Derek Wan <d.wan@icloud.com>
2022-06-10 07:32:32 +0800

    Finish 14.2 almost

--- exercises/rust-by-practice/ch14-2__03/src/lib.rs
+++ exercises/rust-by-practice/ch14-2__03/src/lib.rs
@@ -5,10 +5,10 @@ mod front_of_house {
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
-        fn serve_order() {}
+        pub fn serve_order() {}
 
         fn take_payment() {}
 
@@ -20,4 +20,14 @@ pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
 }
