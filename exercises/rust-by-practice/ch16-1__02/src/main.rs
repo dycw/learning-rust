db52bce93d1798e7c6eb8b281018c23e8dc94e48
Derek Wan <d.wan@icloud.com>
2022-06-10 21:36:35 +0800

    Finish chapter 16

---
+++ exercises/rust-by-practice/ch16-1__02/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    /* Fill in the blanks to make it print:
+    Hello world, I am
+    Sunface!
+    */
+    print!("hello world, ");
+    println!("I am");
+    println!("Sunface!");
+}
