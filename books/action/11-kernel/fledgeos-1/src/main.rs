34c1057055f6c434ba6770eb3c1fb79439b6b74d
Derek Wan <d.wan@icloud.com>
2022-06-18 15:40:33 +0800

    1

--- books/action/11-kernel/fledgeos-1/src/main.rs
+++ books/action/11-kernel/fledgeos-1/src/main.rs
@@ -4,25 +4,23 @@
 
 use core::intrinsics;
 use core::panic::PanicInfo;
-use x86_64::instructions::{hlt};
+use x86_64::instructions::hlt;
 
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-  unsafe {
-    intrinsics::abort();
-  }
+    unsafe {
+        intrinsics::abort();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-  let mut framebuffer = 0xb8000 as *mut u8;
-  unsafe {
-   framebuffer
-    .offset(1)
-    .write_volatile(0x30);
-  }
-  loop {
-    hlt();
-  }
+    let mut framebuffer = 0xb8000 as *mut u8;
+    unsafe {
+        framebuffer.offset(1).write_volatile(0x30);
+    }
+    loop {
+        hlt();
+    }
 }
