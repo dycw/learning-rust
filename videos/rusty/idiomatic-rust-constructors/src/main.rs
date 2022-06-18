58b45add377a0b86c77d74cf6b9690839f230a5e
Derek Wan <d.wan@icloud.com>
2022-06-18 16:20:43 +0800

    Finish

--- videos/rusty/idiomatic-rust-constructors/src/main.rs
+++ videos/rusty/idiomatic-rust-constructors/src/main.rs
@@ -1,3 +1,15 @@
+use idiomatic_rust_constructors::{Post, Role, User};
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User::new("testuser1234".to_string()).unwrap_or_default();
+
+    println!("{:?}", user1);
+
+    let post1 = Post::default();
+
+    println!("{:?}", post1);
+
+    let post2 = Post::new("example content".to_owned());
+
+    println!("{:?}", post2)
 }
